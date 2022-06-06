fa6753d626398c99495232441c477e25e212bbc3
Tamanna-Kalra <83012226+Tamanna-Kalra@users.noreply.github.com>
2022-06-06 17:06:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+}
+
+function draw() {
+    circle(30, 30, 20);
+describe('white circle with black outline in mid of gray canvas');
+    ellipse(56, 46, 55, 55);
+describe('white ellipse with black outline in middle of a gray canvas');
+    rect(30, 20, 55, 55);
+describe('white rect with black outline in mid-right of canvas');
+}
